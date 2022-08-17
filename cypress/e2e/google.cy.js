e848c7f1d4b5599c7c08b9118fb4bf5a672d2e34
Julian O <juospinam@unal.edu.co>
2022-08-16 19:39:52 -0500

    nueva prueba

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -1,6 +1,7 @@
 describe("This is my first cypress test", () => {
     it("should have a title", () => {
       cy.visit("https://www.google.com/");
-      cy.title().should("eq", "Google");
+      cy.get("[title='Buscar']").type("minecraft {enter}")
+      cy.get("h2 span").should("have.text", "Minecraft")
     });
   });
